32becc8db478db5247ef693ab749bde3188d4946
David Guerra <guerra.david717@gmail.com>
2024-02-11 16:49:54 -0500

    POST-DEADLINE - Additional endpoints added

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<DbLogic>(opt => opt.UseSqlServer(connectionString)
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
+
+// GETS
 app.MapGet("/posts", async (DbLogic db) =>
     await db.Posts.ToListAsync());
 
@@ -18,7 +20,66 @@ app.MapGet("/posts/{id}", async (int id, DbLogic db) =>
             ? Results.Ok(posts)
             : Results.NotFound());
 
+app.MapGet("/categories", async (DbLogic db) =>
+    await db.Categories.ToListAsync());
 
-// more endpoints here
+
+// POSTS
+app.MapPost("/posts", async (Post input, DbLogic db) =>
+{ 
+    if (input.Title is null || input.Contents is null) return Results.BadRequest();
+
+    if (await db.Categories.FindAsync(input.Category) is not Category category) return Results.BadRequest();
+
+    Post post = new()
+    {
+        Title = input.Title,
+        Contents = input.Contents,
+        Category = input.Category,
+        Timestamp = DateTime.UtcNow,
+    };
+    db.Posts.Add(post);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/posts/{post.Id}", post);
+});
+
+
+// PUTS
+app.MapPut("/posts/{id}", async (int id, Post input, DbLogic db) =>
+{
+    if (input.Title is null || input.Contents is null) return Results.BadRequest();
+
+    var existing = await db.Posts.FindAsync(id);
+    if (existing is null) return Results.NotFound();
+
+    existing.Title = input.Title;
+    existing.Contents = input.Contents;
+    existing.Category = input.Category;
+
+    await db.SaveChangesAsync();
+    return Results.Accepted($"/posts/{existing.Id}", existing);
+});
+
+
+// DELETES
+app.MapDelete("/posts", async (DbLogic db) =>
+{
+    await db.Posts.ExecuteDeleteAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/posts/{id}", async (int id, DbLogic db) =>
+{
+    if (await db.Posts.FindAsync(id) is Post post)
+    {
+        db.Posts.Remove(post);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
 
 app.Run();
